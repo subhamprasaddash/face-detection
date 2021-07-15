dc37448c463b237d8b36b524de3b7a60d6796c86
subham.dash <subham.dash@xarvis.ai>
2021-07-15 19:45:07 +0530

    version 0.1

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -12,7 +12,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
             <div>
                 <div className = 'form center pa4 br3 shadow-5'>
                 <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
-                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+                    <button className='btn-grad w-30'
                     onClick = {onButtonSubmit} >Detect</button>
                 </div>
             </div>
